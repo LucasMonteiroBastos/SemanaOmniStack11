3641510ddf78d9bbefa0750ffed1203a94bfb7a1
Lucas Monteiro Bastos <monteiro.bastos1994@gmail.com>
2020-03-28 15:34:16 -0300

    orgin2

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -7,10 +7,21 @@ module.exports = {
     connection: {
       filename: './src/database/db.sqlite'
     },
-    migrations:{
-      directory:'./src/database/migrations'
+    migrations: {
+      directory: './src/database/migrations'
     },
-    useNullAsDefault:true,
+    useNullAsDefault: true,
+  },
+
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/test.sqlite'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true,
   },
 
   staging: {
